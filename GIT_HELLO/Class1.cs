c7920b7c84a96c9a6c32628179edb2610f456f1b
PHC <phc@gmail.com>
2022-12-27 17:07:29 +0700

    add class1.css

---
+++ GIT_HELLO/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_HELLO
+{
+    class Class1
+    {
+        // this is class1
+    }
+}
