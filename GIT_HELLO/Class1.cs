8705b3d64e729bfa2ef37ebd5a477a659b2b1e16
PHC <phc@gmail.com>
2022-12-27 17:36:07 +0700

    update class1 class2

--- GIT_HELLO/Class1.cs
+++ GIT_HELLO/Class1.cs
@@ -10,10 +10,8 @@ namespace GIT_HELLO
     {
         // this is class1
         // this is class1
-<<<<<<< HEAD
+
         // this is class1ccc
-=======
-        // this is class111
->>>>>>> 5a2401a05b68c151556c3255a47098d7363fe416
+
     }
 }
